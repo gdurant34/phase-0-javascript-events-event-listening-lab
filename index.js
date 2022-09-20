a3be934654c553c176133380320d07280002174b
Grace Durant <gdurant34@gmail.com>
2022-09-20 15:09:24 -0700

    commit tree 4a9f69578006bbd69a21dc265a8f7c8bd6981200

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+    const clickAlert = () => alert('I was cliecked!');
+    input.addEventListener('click', clickAlert);
+};
